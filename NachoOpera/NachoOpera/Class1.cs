1869123a0653d4e1b2c26ffa13cbe5509b03f177
wolf55421 <jeider.osorio124@pascualbravo.edu.co>
2024-10-19 20:43:33 -0500

    Add files via upload

---
+++ NachoOpera/NachoOpera/Class1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace NachoOpera
+{
+    public class Class1
+    {
+        public static double Suma(double a, double b)
+        {
+            return a + b;
+        }
+
+        public static double Resta(double a, double b)
+        {
+            return a - b;
+        }
+
+        public static double Multiplicacion(double a, double b)
+        {
+            return a * b;
+        }
+
+        public static double Division(double a, double b)
+        {
+            if (b == 0)
+            {
+                throw new DivideByZeroException("No se puede dividir por cero.");
+            }
+            return a / b;
+        }
+    }
+}
